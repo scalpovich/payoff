f0fc7b2d9c81e49705a9350acfb9d87e2c6c7a0a
Galamgam, Ronele <Ronele.Galamgam@misys.com>
2018-11-15 16:06:28 +0800

    Changed old ids to new ids

--- app/src/main/java/com/finastra/rgalamga/myapplication/MainActivity.java
+++ app/src/main/java/com/finastra/rgalamga/myapplication/MainActivity.java
@@ -101,9 +101,9 @@ public class MainActivity extends AppCompatActivity
 
         } else if (id == R.id.nav_manage) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.navSupport) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.navLogut) {
 
         }
 
