3b0b6b363ccb7059ce2afbba978088f5e913e589
aalyanna <alyanna.ascano@finastra.com>
2018-11-16 20:09:50 +0800

    Commit ko for the first time - TAMI

--- app/src/main/java/com/finastra/payments/wifidirect/WiFiDirectBroadcastReceiver.java
+++ app/src/main/java/com/finastra/payments/wifidirect/WiFiDirectBroadcastReceiver.java
@@ -1,10 +1,15 @@
 package com.finastra.payments.wifidirect;
 
+import android.Manifest;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.wifi.p2p.WifiP2pManager;
 import android.net.wifi.p2p.WifiP2pManager.*;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -16,6 +21,8 @@ public class WiFiDirectBroadcastReceiver extends BroadcastReceiver {
     private Channel channel;
     private MainActivity mainActivity;
     PeerListListener myPeerListListener;
+    private static final int PERMISSIONS_REQUEST_CODE_ACCESS_COARSE_LOCATION = 1001;
+
 
     public WiFiDirectBroadcastReceiver(WifiP2pManager wifiP2pManager, WifiP2pManager.Channel channel,
                                        MainActivity activity) {
@@ -41,7 +48,13 @@ public class WiFiDirectBroadcastReceiver extends BroadcastReceiver {
             // Call WifiP2pManager.requestPeers() to get a list of current peers
             Toast.makeText(context,"Requesting peers",Toast.LENGTH_SHORT).show();
             if (wifiP2pManager != null) {
-                wifiP2pManager.requestPeers(channel, mainActivity);
+                if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && ContextCompat.checkSelfPermission(mainActivity,Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+                   ActivityCompat.requestPermissions(mainActivity,new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, PERMISSIONS_REQUEST_CODE_ACCESS_COARSE_LOCATION);
+                    //After this point you wait for callback in onRequestPermissionsResult(int, String[], int[]) overriden method
+                } else{
+                    wifiP2pManager.requestPeers(channel, mainActivity);
+                    //do something, permission was previously granted; or legacy device
+                }
             }
         } else if (WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION.equals(action)) {
             // Respond to new connection or disconnections
