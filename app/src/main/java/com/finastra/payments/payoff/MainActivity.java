3eec736a70754f518c0399afb23fda41ed659faf
Galamgam, Ronele <Ronele.Galamgam@misys.com>
2018-11-16 12:59:07 +0800

    Code for WiFi Direct and change minSDK to 24

--- app/src/main/java/com/finastra/payments/payoff/MainActivity.java
+++ app/src/main/java/com/finastra/payments/payoff/MainActivity.java
@@ -1,10 +1,19 @@
 package com.finastra.payments.payoff;
 
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.wifi.WifiManager;
+import android.net.wifi.p2p.WifiP2pDevice;
+import android.net.wifi.p2p.WifiP2pDeviceList;
+import android.net.wifi.p2p.WifiP2pManager.*;
+import android.net.wifi.p2p.WifiP2pManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.support.design.widget.NavigationView;
@@ -15,31 +24,85 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Button;
-import android.widget.EditText;
+import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import com.finastra.payments.payoff.R;
+import com.finastra.payments.wifidirect.WiFiDirectBroadcastReceiver;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
-    Button buttonOff,buttonDiscover,buttonSend;
-    ListView listView;
-    TextView readMessageBox,connectionStatus;
-    EditText writeMessage;
+    //Instance variables
     private LinearLayout parentLinearLayout;
+
+    WifiP2pManager wifiP2pManager;
+    Channel channel;
+    BroadcastReceiver broadcastReceiver;
+    IntentFilter intentFilter;
+    ListView discoveredPeersList;
+    WifiManager wifiManager;
+    WifiP2pManager.PeerListListener peerListListener;
+
+    List<WifiP2pDevice> peers = new ArrayList<WifiP2pDevice>();
+    String[] deviceNameArray;
+    WifiP2pDevice[] deviceArray;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+        initWifiDirectComponents();
 
+         peerListListener = new WifiP2pManager.PeerListListener() {
+            @Override
+            public void onPeersAvailable (WifiP2pDeviceList peerlist) {
+               Log.i("Main","PEERS AVAILABLE");
+               Log.i("Main",String.valueOf(peerlist.getDeviceList().size()));
+                if(!peerlist.getDeviceList().equals(peers)){
+                    peers.clear();
+                    peers.addAll(peerlist.getDeviceList());
+                    deviceNameArray = new String[peerlist.getDeviceList().size()];
+                    deviceArray = new WifiP2pDevice[peerlist.getDeviceList().size()];
+                    int index = 0;
+                    for (WifiP2pDevice device : peerlist.getDeviceList()) {
+                        deviceNameArray[index] = device.deviceName;
+                        deviceArray[index] = device;
+                        index++;
+                        LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                        View rowView = inflater.inflate(R.layout.field, null);
+                        TextView textView = (TextView) rowView;
+                        Toast.makeText(getApplicationContext(),"Device Name: "+device.deviceName,Toast.LENGTH_SHORT).show();
+                        textView.setText(device.deviceName);
+                        View newView = textView;
+                        // Add the new row before the add field button.
+                        parentLinearLayout.addView(newView, parentLinearLayout.getChildCount() - 1);
+                    }
+                    /*   ArrayAdapter<String> adapter = new ArrayAdapter<String>(getApplicationContext(),android.R.layout.simple_list_item_1,deviceNameArray);
+                     *//*
+                parentLinearLayout.addView(rowView, parentLinearLayout.getChildCount() - 1);*//*
+               // Toast.makeText(getApplicationContext(),deviceNameArray,Toast.LENGTH_SHORT).show();
+                *//*discoveredPeers*//*
+                //parentLinearLayout.setAdapter(adapter);*/
+                }
+                if (peers.size() == 0) {
+                    Toast.makeText(getApplicationContext(),"No device found",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+            }
+        };
+    }
+
+    public void init() {
         Intent intent = new Intent(MainActivity.this,LoginActivity.class);
         startActivity(intent);
-
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
@@ -61,6 +124,20 @@ public class MainActivity extends AppCompatActivity
         navigationView.setNavigationItemSelectedListener(this);
         parentLinearLayout = findViewById(R.id.discoveredPeers);
     }
+
+    public void initWifiDirectComponents() {
+        wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        wifiP2pManager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
+        channel = wifiP2pManager.initialize(this,getMainLooper(),null);
+        broadcastReceiver = new WiFiDirectBroadcastReceiver(wifiP2pManager,channel,this);
+
+        intentFilter = new IntentFilter();
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
+    }
+
 
     @Override
     public void onBackPressed() {
@@ -100,9 +177,9 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.navBankAccount) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.navFxChange) {
 
         } else if (id == R.id.nav_slideshow) {
 
@@ -120,13 +197,6 @@ public class MainActivity extends AppCompatActivity
         return true;
     }
 
-    public void onDiscover(View v) {
-        LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        final View rowView = inflater.inflate(R.layout.field, null);
-        // Add the new row before the add field button.
-        parentLinearLayout.addView(rowView, parentLinearLayout.getChildCount() - 1);
-    }
-
     public void onDelete(View v) {
         parentLinearLayout.removeView((View) v.getParent());
     }
@@ -138,7 +208,49 @@ public class MainActivity extends AppCompatActivity
         String eWalletAmountStr = eWalletAmount.getText() +"";
         double oldValue = Double.parseDouble(eWalletAmountStr);
         double subtractedValue = Double.parseDouble(amountToSendStr);
-        double newBalance = oldValue - subtractedValue;
-        eWalletAmount.setText(newBalance + "");
+        View focusView;
+        if (subtractedValue > oldValue) {
+            Context context = getApplicationContext();
+            CharSequence text = "Amount to send is greater than the balance";
+            int duration = Toast.LENGTH_SHORT;
+            Toast.makeText(context, text, duration).show();
+        } else {
+            double newBalance = oldValue - subtractedValue;
+            eWalletAmount.setText(newBalance + "");
+        }
+    }
+
+    //WiFiDirect
+
+    public void onDiscover(View v) {
+        if(wifiManager.isWifiEnabled()) {
+            wifiManager.setWifiEnabled(true);
+            wifiP2pManager.discoverPeers(channel,new WifiP2pManager.ActionListener(){
+                @Override
+                public void onSuccess() {
+                    //Toast.makeText(getApplicationContext(), "Discovery started", Toast.LENGTH_SHORT).show();
+                }
+
+                @Override
+                public void onFailure(int i) {
+                    Toast.makeText(getApplicationContext(), "Discovery starting failed", Toast.LENGTH_SHORT).show();
+                    //connectionStatus.setText("Discovery starting failed");
+                }
+            });
+        } else {
+            wifiManager.setWifiEnabled(true);
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        registerReceiver(broadcastReceiver,intentFilter);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        unregisterReceiver(broadcastReceiver);
     }
 }
---
+++ app/src/main/java/com/finastra/payments/wifidirect/WiFiDirectBroadcastReceiver.java
@@ -0,0 +1,52 @@
+package com.finastra.payments.wifidirect;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.wifi.p2p.WifiP2pManager;
+import android.net.wifi.p2p.WifiP2pManager.*;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.finastra.payments.payoff.MainActivity;
+
+public class WiFiDirectBroadcastReceiver extends BroadcastReceiver {
+
+    private WifiP2pManager wifiP2pManager;
+    private Channel channel;
+    private MainActivity mainActivity;
+    PeerListListener myPeerListListener;
+
+    public WiFiDirectBroadcastReceiver(WifiP2pManager wifiP2pManager, WifiP2pManager.Channel channel,
+                                       MainActivity activity) {
+        super();
+        this.wifiP2pManager = wifiP2pManager;
+        this.channel = channel;
+        this.mainActivity = activity;
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String action = intent.getAction();
+        Log.i("Main",action);
+        if (WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION.equals(action)) {
+            // Check to see if Wi-Fi is enabled and notify appropriate activity
+            int state = intent.getIntExtra(WifiP2pManager.EXTRA_WIFI_STATE,-1);
+            if (state == WifiP2pManager.WIFI_P2P_STATE_ENABLED) {
+                Toast.makeText(context,"Wifi is ON",Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(context,"Wifi is OFF",Toast.LENGTH_SHORT).show();
+            }
+        } else if (WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION.equals(action)) {
+            // Call WifiP2pManager.requestPeers() to get a list of current peers
+            Toast.makeText(context,"Requesting peers",Toast.LENGTH_SHORT).show();
+            if (wifiP2pManager != null) {
+                wifiP2pManager.requestPeers(channel, myPeerListListener);
+            }
+        } else if (WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION.equals(action)) {
+            // Respond to new connection or disconnections
+        } else if (WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION.equals(action)) {
+            // Respond to this device's wifi state changing
+        }
+    }
+}
